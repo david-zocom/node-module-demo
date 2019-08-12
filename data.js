6539cbf3fa57736edf396b989f4e5640cf709086
David Andersson <David Andersson>
2019-08-12 14:02:45 +0200

    Importera med require

---
+++ data.js
@@ -0,0 +1,10 @@
+
+const array = [
+	12, 23, 767, 0, 93493, 322, 420
+];
+function fun() {}
+
+module.exports = {
+	array: array,
+	boring: fun
+};
---
+++ main.js
@@ -0,0 +1,6 @@
+// Här startar programmet
+
+console.log('Helloooo');
+
+const data = require('./data.js');
+console.log('Datan är: ', data);
